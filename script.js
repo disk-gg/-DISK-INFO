ba6c68ad7d90d5005e5d4fa2bc826a088b85dfb6
disk.gg <newnewkay3@gmail.com>
2026-05-18 21:14:18 -0300

    mudanças

--- script.js
+++ script.js
@@ -46,9 +46,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Botões de navegação
     const buttons = [
-        { id: 'home', name: 'INI' },
-        { id: 'team', name: 'TEAM' },
-        { id: 'servers', name: '/DISK OPEN' }
+        { id: 'INI', name: 'INI' },
+        { id: 'TEAM', name: 'TEAM' },
+        { id: '/DISK OPEN', name: '/DISK OPEN' }
     ];
     
     buttons.forEach(btn => {
